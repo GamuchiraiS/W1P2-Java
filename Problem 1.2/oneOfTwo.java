3d70d46cbc870ae686b457a80e79125fccdb41de
GamuchiraiS <80484981+GamuchiraiS@users.noreply.github.com>
2021-10-21 16:55:15 +0200

    Add files via upload

---
+++ Problem 1.2/oneOfTwo.java
@@ -0,0 +1,31 @@
+//For scanner 
+import java.util.*;
+import java.lang.*;
+
+public class oneOfTwo {
+	public static void main(String [] args){
+
+		int firstInt; 
+		int secondInt; 
+		int thirdInt;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Please enter a number: ");
+		firstInt = Integer.parseInt(sc.nextLine());
+
+		System.out.println("Please enter a number: ");
+		secondInt = Integer.parseInt(sc.nextLine());
+
+		System.out.println("Please enter a number: ");
+		thirdInt = Integer.parseInt(sc.nextLine());
+
+		System.out.println("The maximum value is: " + 
+		Math.max(firstInt, Math.max(secondInt, thirdInt)));
+
+		System.out.println("The minimum value is: " + 
+		Math.min(firstInt, Math.min(secondInt, thirdInt)));
+	}
+
+
+}
